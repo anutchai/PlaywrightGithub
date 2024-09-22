6b1dc617d7c51c77965ffb3a63d775c99294dfb4
Anutchai <anuschai.ch@rmuti.ac.th>
2024-09-22 22:27:51 +0700

    update : workflow

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/1');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
